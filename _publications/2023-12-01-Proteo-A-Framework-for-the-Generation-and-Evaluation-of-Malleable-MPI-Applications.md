489e40331d2ba615bbb594996aa00634464cd47b
siserte <sergioiserte@gmail.com>
2024-04-07 09:21:19 +0200

    remove and create dirs map talks and pubs

--- _publications/2023-12-01-Proteo-A-Framework-for-the-Generation-and-Evaluation-of-Malleable-MPI-Applications.md
+++
@@ -1,9 +0,0 @@
----
-title: "Proteo: A Framework for the Generation and Evaluation of Malleable MPI Applications"
-collection: publications
-permalink: /publication/2023-12-01-Proteo-A-Framework-for-the-Generation-and-Evaluation-of-Malleable-MPI-Applications
-type: "journal"
-date: 2023-12-01
-venue: '<em>Journal of Supercomputing</em>(), pp. -'
-citation: ' I. Martín-Álvarez,  J. Aliaga,  M. Castillo, and  <strong>S. Iserte</strong>, &quot;Proteo: A Framework for the Generation and Evaluation of Malleable MPI Applications.&quot; <em>Journal of Supercomputing</em>(), pp. -, Dec. 2023.'
----
--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,1 +1,112 @@
-var addressPoints = [];
+var addressPoints = [
+  [
+    "San Francisco, California",
+    37.7792588,
+    -122.4193286
+  ],
+  [
+    "Madrid, Spain",
+    40.4167047,
+    -3.7035825
+  ],
+  [
+    "Valladolid, Spain",
+    41.6521328,
+    -4.728562
+  ],
+  [
+    "Lugano, Switzerland",
+    46.0050102,
+    8.9520281
+  ],
+  [
+    "Paris, France",
+    48.8588897,
+    2.3200410217200766
+  ],
+  [
+    "Barcelona, Spain",
+    41.3828939,
+    2.1774322
+  ],
+  [
+    "Castell\u00f3 de la Plana, Spain",
+    39.9860347,
+    -0.0377354
+  ],
+  [
+    "C\u00f3rdoba, Spain",
+    37.8845813,
+    -4.7760138
+  ],
+  [
+    "Timisoara, Romania",
+    45.7538355,
+    21.2257474
+  ],
+  [
+    "Rome, Italy",
+    41.8933203,
+    12.4829321
+  ],
+  [
+    "Bristol, United Kingdom",
+    51.4538022,
+    -2.5972985
+  ],
+  [
+    "M\u00e1laga, Spain",
+    36.7213028,
+    -4.4216366
+  ],
+  [
+    "Teruel, Spain",
+    40.6005259,
+    -0.9229281876827901
+  ],
+  [
+    "Limassol, Cyprus",
+    34.6852901,
+    33.0332657
+  ],
+  [
+    "Rennes, France",
+    48.1113387,
+    -1.6800198
+  ],
+  [
+    "Frankfurt, Germany",
+    50.1106444,
+    8.6820917
+  ],
+  [
+    "Online",
+    43.59047185,
+    3.8595132132013186
+  ],
+  [
+    "Politechnika Cz\u0119stochowska, Poland",
+    50.822852350000005,
+    19.115807327479168
+  ],
+  [
+    "Bordeaux, France",
+    44.841225,
+    -0.5800364
+  ],
+  [
+    "Ciudad Real, Spain",
+    38.959734749999996,
+    -3.8828744402485436
+  ],
+  [
+    "Denver, Colorado, USA",
+    39.7392364,
+    -104.984862
+  ],
+  [
+    "Kobe, Japan",
+    34.6932379,
+    135.1943764
+  ]
+];
